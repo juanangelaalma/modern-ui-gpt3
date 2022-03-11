a84220927895dabbd9cb1e4c49119b01c120e79a
juanangelaalma <juanlah77@gmail.com>
2022-03-11 23:37:46 +0700

    add whatgpt3

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './feature.css'
 
-const Feature = () => {
+const Feature = ({ text, title }) => {
   return (
-    <div>Feature</div>
+    <div className='gpt3__feature-container__feature'>
+      <div className='gpt3__feature-container__feature-title'>
+        <div />
+        <h1>{ title } </h1>
+      </div>
+      <div className="gpt3__feature-container_feature-text">
+        <p>{ text }</p>
+      </div>
+    </div>
   )
 }
 

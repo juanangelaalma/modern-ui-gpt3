00eb415cbbb0224b31034fdcff91f8e31b698655
juanangelaalma <juanlah77@gmail.com>
2022-03-13 06:12:11 +0700

    add future now

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import './features.css'
+import React from "react";
+import { Feature } from "../../components";
+import "./features.css";
 
 const Features = () => {
   return (
-    <div>Features</div>
-  )
-}
+    <div className="gpt3__features section__padding" id="features">
+      <div className="gpt3__features-title">
+        <h1 className="gradient__text">
+          The Future is Now and You Just Need To Realize It. Step into Future
+          Today & Make it Happen.
+        </h1>
+        <p>Request Early Access to Get Started</p>
+      </div>
+      <div className="gpt3__features-container">
+        <Feature
+          title="Improving end distrusts instantly "
+          text="From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded."
+        />
+        <Feature
+          title="Improving end distrusts instantly "
+          text="From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded."
+        />
+        <Feature
+          title="Improving end distrusts instantly "
+          text="From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded."
+        />
+        <Feature
+          title="Improving end distrusts instantly "
+          text="From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded."
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Features
+export default Features;

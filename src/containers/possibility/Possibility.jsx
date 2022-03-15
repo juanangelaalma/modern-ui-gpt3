f7bcb790044a248ff4deafd5a06e2d15303e9589
juanangelaalma <juanlah77@gmail.com>
2022-03-15 18:37:34 +0700

    add footer possibility

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -5,14 +5,25 @@ import possibility from "../../assets/possibility.png"
 const Possibility = () => {
   return (
     <div className='gpt3__possibility section__margin'>
-      <div className='gpt3__possibility-image'>
-        <img src={possibility} alt="Possibility" />
+      <div className="gpt3__possibility-header">
+        <div className='gpt3__possibility-header_image'>
+          <img src={possibility} alt="Possibility" />
+        </div>
+        <div className='gpt3__possibility-header_content'>
+          <span>Request Early Access to Get Started</span>
+          <h1 className='gpt3__possibility-header_content-title gradient__text'>The possibilities are beyond your imagination</h1>
+          <p className='gpt3__possibility-header_content-text'>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+          <p>Request Early Access to Get Started</p>
+        </div>
       </div>
-      <div className='gpt3__possibility-content'>
-        <span>Request Early Access to Get Started</span>
-        <h1 className='gpt3__possibility-content_title gradient__text'>The possibilities are beyond your imagination</h1>
-        <p className='gpt3__possibility-content_text'>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
-        <p>Request Early Access to Get Started</p>
+      <div className='gpt3__possibility-footer'>
+        <div className="gpt3__possibility-footer_text">
+          <p>Request Early Access to Get Started</p>
+          <h1>Register today & start exploring the endless possiblities.</h1>
+        </div>
+        <div className='gpt3__possibility-footer_button'>
+          <button>Get Started</button>
+        </div>
       </div>
     </div>
   )
